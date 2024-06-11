52b04828efdb0d89466fa84ba9aabbbcca954bd4
Ruthvik <ruthviksaikumar240405@gmail.com>
2024-06-11 16:44:12 +0530

    Charts

--- graphics/main_window.py
+++ graphics/main_window.py
@@ -1,3 +1,5 @@
+import PySide6.QtCharts as QtCharts
+from PySide6.QtGui import QPainter, Qt, QIcon
 from PySide6.QtWidgets import QMainWindow, QPushButton, QGridLayout, QWidget, QSizePolicy, QScrollArea, QTabWidget
 
 
@@ -8,27 +10,78 @@ class MainWindow(QMainWindow):
         self.setWindowTitle("ReConnect - Productivity Tracker")
         self.setGeometry(200, 100, 600, 600)
         self.setMinimumSize(650, 600)
+        icon = QIcon("assets/ReConnect Logo.png")
+        self.setWindowIcon(icon)
 
         ###############################################################################################
 
-        self.button1 = QPushButton("Button 1")
-        self.button2 = QPushButton("Button 2")
+        self.pie = QtCharts.QPieSeries()
+        self.pie.append("Work", 10)
+        self.pie.append("Study", 20)
+        self.pie.append("Leisure", 30)
+        self.pie.append("Sleep", 40)
+
+        self.pie.setLabelsVisible(True)
+        self.pie.setHoleSize(0.5)
+
+        self.chart = QtCharts.QChart()
+        self.chart.addSeries(self.pie)
+        self.chart.setAnimationOptions(QtCharts.QChart.AnimationOption.SeriesAnimations)
+        self.chart.setTheme(QtCharts.QChart.ChartTheme.ChartThemeDark)
+        self.chart.setBackgroundRoundness(10)
+
+        self.chart_view = QtCharts.QChartView(self.chart)
+        self.chart_view.setRenderHint(QPainter.RenderHint.Antialiasing)
+
+        ########################################
+
+        self.bar = QtCharts.QBarSet("Minutes")
+
+        categories = ["Work", "Study", "Leisure", "Sleep"]
+        minutes = [10, 20, 30, 40]
+        for category, minute in zip(categories, minutes):
+            self.bar.append(minute)
+
+        self.bar_series = QtCharts.QHorizontalBarSeries()
+        self.bar_series.append(self.bar)
+
+        self.bar_chart = QtCharts.QChart()
+        self.bar_chart.addSeries(self.bar_series)
+        self.bar_chart.setAnimationOptions(QtCharts.QChart.AnimationOption.SeriesAnimations)
+        self.bar_chart.setTheme(QtCharts.QChart.ChartTheme.ChartThemeDark)
+        self.bar_chart.setBackgroundRoundness(10)
+        self.bar_chart.legend().hide()
+
+        self.axis_x = QtCharts.QValueAxis()
+        self.axis_x.setLabelFormat("%i")
+        self.axis_x.setTitleText("Minutes")
+        self.bar_chart.setAxisX(self.axis_x, self.bar_series)
+
+        self.axis_y = QtCharts.QBarCategoryAxis()
+        self.axis_y.append(categories)
+        self.bar_chart.setAxisY(self.axis_y, self.bar_series)
+
+        self.bar_chart_view = QtCharts.QChartView(self.bar_chart)
+        self.bar_chart_view.setRenderHint(QPainter.RenderHint.Antialiasing)
+
+        ############################################
+
         self.button3 = QPushButton("Button 3")
         self.button4 = QPushButton("Button 4")
 
-        self.button1.setSizePolicy(QSizePolicy.Policy.Expanding, QSizePolicy.Policy.Expanding)
-        self.button2.setSizePolicy(QSizePolicy.Policy.Expanding, QSizePolicy.Policy.Expanding)
+        self.chart_view.setSizePolicy(QSizePolicy.Policy.Expanding, QSizePolicy.Policy.Expanding)
+        self.bar_chart_view.setSizePolicy(QSizePolicy.Policy.Expanding, QSizePolicy.Policy.Expanding)
         self.button3.setSizePolicy(QSizePolicy.Policy.Expanding, QSizePolicy.Policy.Expanding)
         self.button4.setSizePolicy(QSizePolicy.Policy.Expanding, QSizePolicy.Policy.Expanding)
 
-        self.button1.setMinimumSize(300, 300)
-        self.button2.setMinimumSize(300, 300)
+        self.chart_view.setMinimumSize(300, 300)
+        self.bar_chart_view.setMinimumSize(300, 300)
         self.button3.setMinimumSize(300, 300)
         self.button4.setMinimumSize(300, 300)
 
         self.grid = QGridLayout()
-        self.grid.addWidget(self.button1, 0, 0)
-        self.grid.addWidget(self.button2, 0, 1)
+        self.grid.addWidget(self.chart_view, 0, 0)
+        self.grid.addWidget(self.bar_chart_view, 0, 1)
         self.grid.addWidget(self.button3, 1, 0)
         self.grid.addWidget(self.button4, 1, 1)
 
@@ -41,14 +94,12 @@ class MainWindow(QMainWindow):
 
         ###############################################################################################
 
-        self.button8 = QPushButton("Button 8")
-
-        self.button8.setSizePolicy(QSizePolicy.Policy.Expanding, QSizePolicy.Policy.Expanding)
-
-        self.button8.setMinimumSize(300, 300)
+        self.button5 = QPushButton("Button 1")
+        self.button5.setSizePolicy(QSizePolicy.Policy.Expanding, QSizePolicy.Policy.Expanding)
+        self.button5.setMinimumSize(300, 300)
 
         self.grid2 = QGridLayout()
-        self.grid2.addWidget(self.button8, 0, 0)
+        self.grid2.addWidget(self.button5, 0, 0)
 
         central_widget2 = QWidget()
         central_widget2.setLayout(self.grid2)
