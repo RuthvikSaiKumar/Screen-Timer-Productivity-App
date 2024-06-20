6463be895598217f52c27ed95958320cb515a367
Code-Monish <114045642+Code-Monish@users.noreply.github.com>
2024-06-20 21:36:08 +0530

    encryption added

--- application_timer/window_utils.py
+++ application_timer/window_utils.py
@@ -9,15 +9,13 @@ import pygetwindow as gw
 import time
 from datetime import date
 import datetime
-import datetime
 import pickle
 import psutil
 import pandas as pd
+import json
+from cryptography.fernet import Fernet
+import os
 
-
-# from user_agents import parse
-
-# This class is supposed to give the main file, the data on current window. 
 class WindowUtils:
 
     def __init__(self):
@@ -29,12 +27,17 @@ class WindowUtils:
         self.end_time = 0
         self.save_path = 'window_screentime_cache.pkl'
         self.last_save_time = time.time()
-        # self.browser = user_agent.browser.family
-        self.browser_list = ["Chrome", "Firefox", "Edge", "Safari", "Opera", "Internet Explorer", "Vivaldi", "Brave",
-                             "Torch", "Yandex Browser", "Maxthon", "Coc Coc", "Comodo Dragon", "Epic Privacy Browser",
-                             "Waterfox", "Pale Moon", "SeaMonkey", "Tor Browser", "Brave Privacy Browser", "K-Meleon"]
+        self.key = None
 
-    # This method will enable us to call this function and make a save of data for each session and renew the dict.
+        # Load the encryption key from the file
+        if os.path.exists('KEY'):
+            with open('KEY', 'rb') as k:
+                self.key = k.read()
+        else:
+            self.key = Fernet.generate_key()
+            with open('KEY', 'wb') as k:
+                k.write(self.key)
+
     def refresh_data(self):
         self.window_screentime = {}
 
@@ -42,22 +45,29 @@ class WindowUtils:
         # Save the data in a temp file for the session every 2min.
         current_time = time.time()
         if current_time - self.last_save_time >= 120 or interrupt:
-            with open(self.save_path, "wb") as f:
-                pickle.dump(self.window_screentime, f)
+            self.encrypt_and_save_data()
             self.window_screentime = {}
             self.last_save_time = current_time
 
+    def encrypt_and_save_data(self):
+        encrypted_data = self.encrypt_dict(self.window_screentime)
+        with open(self.save_path, "wb") as f:
+            f.write(encrypted_data)
+
+    def encrypt_dict(self, data):
+        json_data = json.dumps(data)
+        encrypted_text = Fernet(self.key).encrypt(json_data.encode())
+        return encrypted_text
+
+    def decrypt_and_load_data(self):
+        with open(self.save_path, "rb") as f:
+            encrypted_data = f.read()
+        decrypted_data = Fernet(self.key).decrypt(encrypted_data)
+        decrypted_str = decrypted_data.decode()
+        self.window_screentime = json.loads(decrypted_str)
+
     def data_parse(self):
         return self.window_screentime
-
-    def get_app_type(self, window):
-        with (contextlib.suppress(KeyboardInterrupt)):
-            for process in psutil.process_iter(['pid', 'name']):
-                with contextlib.suppress(psutil.NoSuchProcess, psutil.AccessDenied, psutil.ZombieProcess):
-                    if process.info['name'].lower() in [i.lower() + '.exe' for i in self.browser_list] and \
-                            window.title in process.as_dict()['cmdline']:
-                        return "browser"
-        return "application"
 
     def process_data(self):
         model = WindowsModel(self.window_screentime)
@@ -70,21 +80,15 @@ class WindowUtils:
             # handling the exceptions to keyboard interruption
             try:
                 window = gw.getActiveWindow()
-                if self.previous_window != window:
+                if self.previous_window!= window:
                     if self.previous_window is not None:
                         self.end_time = time.time()
                         window_time = self.end_time - self.start_time
                         h, m, s = int(window_time // 3600), int(window_time % 3600) // 60, int(window_time % 60)
                         print("Time spent on ", self.previous_window.title, " is: ", f"{h:02}:{m:02}:{s:02}")
-                        window_time_hhmmss = time.strftime("%H:%M:%S", time.gmtime(window_time))
-                        app_type = self.get_app_type(self.previous_window)
-                        self.window_screentime[self.previous_window.title] = {
-                            "apptype": app_type,  # or "application"
-                            "time_spent": {
-                                "hours": int(window_time_hhmmss.split(":")[0]),
-                                "minutes": int(window_time_hhmmss.split(":")[1]),
-                                "seconds": int(window_time_hhmmss.split(":")[2]),
-                            }
+                        self.window_screentime.setdefault(str(self.today), {})[self.previous_window.title] = {
+                            "time_spent": window_time,
+                            "tabs": {}
                         }
                         self.save_data()
 
@@ -99,15 +103,9 @@ class WindowUtils:
                     window_time = self.end_time - self.start_time
                     h, m, s = int(window_time // 3600), int(window_time % 3600) // 60, int(window_time % 60)
                     print("Time spent on ", self.previous_window.title, " is: ", f"{h:02}:{m:02}:{s:02}")
-                    window_time_hhmmss = time.strftime("%H:%M:%S", time.gmtime(window_time))
-                    app_type = self.get_app_type(self.previous_window)
-                    self.window_screentime[self.previous_window.title] = {
-                        "apptype": app_type,  # or "application"
-                        "time_spent": {
-                            "hours": int(window_time_hhmmss.split(":")[0]),
-                            "minutes": int(window_time_hhmmss.split(":")[1]),
-                            "seconds": int(window_time_hhmmss.split(":")[2]),
-                        }
+                    self.window_screentime.setdefault(str(self.today), {})[self.previous_window.title] = {
+                        "time_spent": window_time,
+                        "tabs": {}
                     }
                     print(self.window_screentime)
                 self.save_data(interrupt=True)
@@ -125,9 +123,4 @@ class WindowsModel:
         folder_path = 'user-data'
         file_path = os.path.join(folder_path, self.file_name)
         df.to_pickle(file_path)
-        print(f"Data saved to {file_path}")
-
-
-# TODO : Implement a way to avoid loss of data from certain applications due to the limits of pygetwindow TODO : Make
-#  the code for data_traverse such that it reads the data from the cache completely and store it in a dict which is
-#  then shared to another file.
+       
