c6760bcba6478e08caead6adcb39c6780d7a0e3f
Code-Monish <114045642+Code-Monish@users.noreply.github.com>
2024-06-30 14:30:58 +0530

    Removed redundant folder

--- application_timer/test.py
+++
@@ -1,9 +0,0 @@
-import psutil
-
-# Get a list of all running process
-for proc in psutil.process_iter(['pid', 'name']):
-    try:
-        info = proc.info
-        print(f"Process ID: {info['pid']} - Process Name: {info['name']}")
-    except (psutil.NoSuchProcess, psutil.AccessDenied, psutil.ZombieProcess):
-        pass
